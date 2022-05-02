21e519f0860fff8f01a7e7ea15b71560b9f22970
expldec <explosive.deconstruction@gmail.com>
2022-05-02 15:07:47 +0200

    achieved milestone 2 and bonus task

--- data.js
+++ data.js
@@ -113,15 +113,53 @@ const data = [
 	}
 ];
 
-data.forEach(element => {
-	const newCell = document.createElement('div');
-	newCell.classList.add('box');
-	const cellIcon = document.createElement('i') ;
-	cellIcon.classList.add(element.family);
-	cellIcon.classList.add(`${element.prefix}${element.name}`)
-	newCell.append(cellIcon)
-	const cellText = document.createElement('p');
-	cellText.textContent = element.name.toUpperCase();
-	newCell.append(cellText);
-	document.querySelector('.grid-container').append(newCell);
-});
+//per popolare la pagina all'esecuzione dello script, chiamiamo la funzione drawGrid
+drawGrid('data');
+
+
+// funzione drawGrid
+// prende un array di oggetti che hanno le key name, prefix, family e color corrispondenti a icone di FontAwesome
+// e li usa per costruire un div con class 'grid-container' contenente tanti div con classe 'box' quanti gli oggetti nell'array.
+// i div contengono l'icona e un paragrafo che contiene il nome.
+// il grid-container così creato viene poi piazzato nella pagina sostituendo il grid-container esistente.
+function drawGrid(iconArray) {
+	//imbrigliamo il grid-container esistente 
+	let grid = document.querySelector('.grid-container');
+	//creiamo un grid-container pulito
+    let newGrid = document.createElement('div');
+	newGrid.classList.add('grid-container');
+	//per ogni oggetto nell'array
+	iconArray.forEach(element => {
+		//creiamo un div con classe box
+		const newCell = document.createElement('div');
+		newCell.classList.add('box');
+		// creiamo un i e gli mettiamo le classi necessarie partendo dalle key dell'oggetto.
+		const cellIcon = document.createElement('i') ;
+		cellIcon.classList.add(element.family);
+		cellIcon.classList.add(`${element.prefix}${element.name}`)
+		//aggiungiamo il colore come stile css inline
+		cellIcon.style.color = element.color;
+		//appendiamo l'i al box
+		newCell.append(cellIcon)
+		//creaimo un paragrafo con il nome dell'oggetto e appendiamo anche lui
+		const cellText = document.createElement('p');
+		cellText.textContent = element.name.toUpperCase();
+		newCell.append(cellText);
+		//la nostra cella è pronta, la appendiamo al grid-container
+		newGrid.append(newCell);
+	});
+    //sostituiamo il grid-container esistente con quello generato dal forEach
+    grid.parentNode.replaceChild(newGrid, grid);
+}
+
+// questa funzione legge il valore del select e lo usa per filtrare l'array 'data'
+// usa l'array filtrato per ridisegnare la griglia 
+function selectFilter() {
+	filterValue = this.value;
+	// aggiungiamo elementi all'array filtrato solo se la key "type" dell'oggetto è "all" o uguale al filterValue
+	let filteredData = data.filter((element) => filterValue === 'all' || filterValue === element.type);
+	drawGrid(filteredData);
+}
+
+//aggiungiamo un event listener che chiama la funzione selectFilter quando l'utente modifica il valore del select
+document.querySelector('#icon-select').addEventListener('input', selectFilter)
